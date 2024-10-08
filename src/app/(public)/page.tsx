e4b51ac12e878572b1087abb84863e42d967a003
Eun Oh <bdohhhhh@gmail.com>
2024-10-08 21:29:30 +0900

    [update]: prefetch 추가

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,7 +1,26 @@
+import { getProjects } from "@/apis/projects";
+import { QUERY_KEY_PROJECTS } from "@/constants/query.constants";
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import { Suspense } from "react";
 import MainComponent from "./_components/MainComponent";
 
-function MainPage() {
-    return <MainComponent />;
+async function MainPage() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: [QUERY_KEY_PROJECTS],
+    queryFn: () => getProjects(),
+  });
+
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <HydrationBoundary state={dehydratedState}>
+        <MainComponent />
+      </HydrationBoundary>
+    </Suspense>
+  );
 }
 
 export default MainPage;
