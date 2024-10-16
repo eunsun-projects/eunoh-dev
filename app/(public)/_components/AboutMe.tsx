87f530dc6f67d9c69d4f42f5599489bcc1685421
EunOh <bdohhhhh@gmail.com>
2024-10-16 14:15:15 +0900

    [update]: 기본정보 수정

--- app/(public)/_components/AboutMe.tsx
+++ app/(public)/_components/AboutMe.tsx
@@ -30,14 +30,12 @@ function AboutMe() {
         </div>
         <div className="flex flex-col justify-start gap-6 w-full h-[80%] xl:gap-2 xl:px-2">
           <div className="flex flex-col items-start justify-center gap-4 h-[calc(100%/3)]">
-            <h3 className="font-bold text-2xl xl:text-[1.7rem]">{"> 기본기에 충실한 개발자"}</h3>
+            <h3 className="font-bold text-2xl xl:text-[1.7rem]">{"> 꾸준히 성장하는 개발자"}</h3>
 
             <p className="text-base xl:text-lg">
               <ReactTyped
                 startWhenVisible
-                strings={[
-                  "문제를 해결하고 새로운 기술을 습득하는 것을 좋아하며, 항상 기본기에 충실하고자 합니다.",
-                ]}
+                strings={["200개 이상의 TIL과 3500개 이상의 커밋, 지속적으로 성장하는 개발자입니다."]}
                 typeSpeed={10}
               />
             </p>
@@ -47,9 +45,7 @@ function AboutMe() {
             <p className="text-base xl:text-lg">
               <ReactTyped
                 startWhenVisible
-                strings={[
-                  "팀원들과 소통하고 협업하는 것을 즐깁니다. 무엇보다 중요한 것은 커뮤니케이션 능력이라고 생각합니다.",
-                ]}
+                strings={["3회 이상의 프로젝트 리더 경험, 동기부여를 통해 함께 성장하는 개발자입니다."]}
                 typeSpeed={10}
               />
             </p>
@@ -60,7 +56,7 @@ function AboutMe() {
               <ReactTyped
                 startWhenVisible
                 strings={[
-                  "항상 사용자의 관점에서 생각하며, 더욱 편리하고 매력적인 인터페이스를 제공하기 위해 노력하고 있습니다.",
+                  "102개의 피드백 반영 경험, 더욱 편리하고 매력적인 인터페이스를 제공하기 위해 노력하고 있습니다.",
                 ]}
                 typeSpeed={10}
               />
