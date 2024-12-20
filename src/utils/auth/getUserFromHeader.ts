0e4ab8be67d20d8869698fea25942516c9de663c
Eun Oh <bdohhhhh@gmail.com>
2024-12-20 21:25:42 +0900

    [update]: headers next 15 사용법에 맞게 업데이트

--- src/utils/auth/getUserFromHeader.ts
+++ src/utils/auth/getUserFromHeader.ts
@@ -1,7 +1,7 @@
 import { headers } from 'next/headers';
 
-export const getUserFromHeader = () => {
-  const headersList = headers();
+export const getUserFromHeader = async () => {
+  const headersList = await headers();
   const user = headersList.get('x-user');
 
   return user;
