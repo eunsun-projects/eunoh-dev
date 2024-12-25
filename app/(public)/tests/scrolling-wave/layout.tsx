8070c65b4426dd82e7eb3cb7ac040f4d43d6aad1
Eun Oh <bdohhhhh@gmail.com>
2024-12-25 21:56:38 +0900

    [add]: tests / scrolling-wave

---
+++ app/(public)/tests/scrolling-wave/layout.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next';
+import { PropsWithChildren } from 'react';
+
+export const metadata: Metadata = {
+  title: 'ScrollingWave',
+  description: 'ScrollingWave',
+};
+
+export default function ScrollingWaveLayout({ children }: PropsWithChildren) {
+  return <section className="absolute top-0 left-0 bg-neutral-400 z-50">{children}</section>;
+}
