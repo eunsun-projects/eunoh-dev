31575b0d141922fe6174d5b868e6b3547026fc9a
Eun Oh <bdohhhhh@gmail.com>
2024-12-27 09:17:38 +0900

    [add]: tests / circles

---
+++ app/(public)/tests/circles/layout.tsx
@@ -0,0 +1,5 @@
+import { PropsWithChildren } from 'react';
+
+export default function CirclesLayout({ children }: PropsWithChildren) {
+  return <div className="fixed inset-0 h-screen w-screen bg-white z-50">{children}</div>;
+}
---
+++ app/(public)/tests/circles/page.tsx
@@ -0,0 +1,19 @@
+import { Metadata, Viewport } from 'next';
+import CirclesTemplate from './_components/CirclesTemplate';
+
+export const metadata: Metadata = {
+  title: 'Circles',
+  description: 'Circles',
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  viewportFit: 'cover',
+};
+
+export default function CirclesPage() {
+  return <CirclesTemplate />;
+}
