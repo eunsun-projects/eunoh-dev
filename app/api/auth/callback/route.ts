13fc72d32cc1e9881ec43c1554282d00dad90fcb
EunOh <bdohhhhh@gmail.com>
2025-05-24 16:29:21 +0900

    [update]: auth callback route

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
   // if "next" is in param, use it as the redirect URL
-  const next = searchParams.get("next") ?? "/";
+  const next = searchParams.get("next") ?? "/admin";
 
   if (code) {
     const cookieStore = await cookies();
@@ -22,28 +22,28 @@ export async function GET(request: Request) {
           setAll(cookiesToSet) {
             try {
               cookiesToSet.forEach(({ name, value, options }) =>
-                cookieStore.set(name, value, options),
+                cookieStore.set(name, value, options)
               );
             } catch {}
           },
         },
-      },
+      }
     );
     const { data, error } = await supabase.auth.exchangeCodeForSession(code);
 
     if (!error) {
-      console.log("data ===========>", data);
+      // console.log("data ===========>", data);
       const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === "development";
       if (isLocalEnv) {
         // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-        return NextResponse.redirect(`${origin}/admin`);
+        return NextResponse.redirect(`${origin}${next}`);
       }
       if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}/admin`);
+        return NextResponse.redirect(`https://${forwardedHost}${next}`);
       }
       // return NextResponse.redirect(`${origin}${next}`);
-      return NextResponse.redirect(`${origin}/admin`);
+      return NextResponse.redirect(`${origin}${next}`);
     }
   }
 
