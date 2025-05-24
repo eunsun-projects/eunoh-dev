47670a8c9c6bc1fb35ab4be70e10e990456866a1
EunOh <bdohhhhh@gmail.com>
2025-05-24 16:28:08 +0900

    [update]: TimeCapsuleTemplate

--- app/(public)/tests/timecapsule/_components/TimeCapsuleTemplate.tsx
+++ app/(public)/tests/timecapsule/_components/TimeCapsuleTemplate.tsx
@@ -1,22 +1,27 @@
-'use client';
+"use client";
 
-import Loading from '@/app/loading';
-import dynamic from 'next/dynamic';
-import { useSearchParams } from 'next/navigation';
-import { Suspense, useEffect } from 'react';
-import { useTimeCapsulesQuery } from '../_hooks/timecapsule.hooks';
-import { useTimeCapsuleStore } from '../_libs/zustand';
-import TimeCapsuleUI from './TimeCapsuleUI';
+import Loading from "@/app/loading";
+import dynamic from "next/dynamic";
+import { useSearchParams } from "next/navigation";
+import { Suspense, useEffect } from "react";
+import { useTimeCapsulesQuery } from "../_hooks/timecapsule.hooks";
+import { useTimeCapsuleStore } from "../_libs/zustand";
+import TimeCapsuleUI from "./TimeCapsuleUI";
 
-const TimeCapsuleCanvas = dynamic(() => import('./TimeCapsuleCanvas'), {
+const TimeCapsuleCanvas = dynamic(() => import("./TimeCapsuleCanvas"), {
   ssr: false,
 });
 
 function TimeCapsuleTemplate() {
   const searchParams = useSearchParams();
-  const queryStringTimeCapsuleId = searchParams.get('id');
-  const { data: timeCapsulesFromSupabase, isPending, error } = useTimeCapsulesQuery();
-  const { setTimeCapsulesWithoutObject, setQueryStringTimeCapsuleId } = useTimeCapsuleStore();
+  const queryStringTimeCapsuleId = searchParams.get("id");
+  const {
+    data: timeCapsulesFromSupabase,
+    isPending,
+    error,
+  } = useTimeCapsulesQuery();
+  const { setTimeCapsulesWithoutObject, setQueryStringTimeCapsuleId } =
+    useTimeCapsuleStore();
 
   useEffect(() => {
     if (!timeCapsulesFromSupabase) return;
