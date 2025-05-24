5f741e091e0f1b631ac3b4b44ce0875ed09ef500
EunOh <bdohhhhh@gmail.com>
2025-05-24 23:48:28 +0900

    [update]: UsageCalculatorTemplate

--- app/(public)/tests/usage-calculator/_components/UsageCalculatorTemplate.tsx
+++ app/(public)/tests/usage-calculator/_components/UsageCalculatorTemplate.tsx
@@ -2,12 +2,14 @@
 
 import CalculatorHeader from "./CalculatorHeader";
 import ModeSelector from "./ModeSelector";
+import ModelSelector from "./ModelSelector";
 
 function UsageCalculatorTemplate() {
   return (
     <div className="flex flex-col gap-4 justify-center items-center">
       <CalculatorHeader />
       <ModeSelector />
+      <ModelSelector />
     </div>
   );
 }
