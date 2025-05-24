d41b6fa7c928c8faa932ee64fa8c011963ca456c
EunOh <bdohhhhh@gmail.com>
2025-05-24 16:27:51 +0900

    [update]: admin header

--- app/(admin)/admin/_components/AdminHeader.tsx
+++ app/(admin)/admin/_components/AdminHeader.tsx
@@ -38,7 +38,7 @@ function AdminHeader() {
           <button
             type="button"
             className="bg-blue-500 text-white px-4 py-2 rounded-md"
-            onClick={() => loginWithProvider("google")}
+            onClick={() => loginWithProvider("google", "/admin")}
           >
             구글로그인
           </button>
