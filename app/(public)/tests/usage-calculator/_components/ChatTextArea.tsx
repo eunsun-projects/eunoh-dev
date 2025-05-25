bedf696e7c41841c8fd4c881a7c80df305a14dcb
Eun Oh <bdohhhhh@gmail.com>
2025-05-25 21:09:38 +0900

    [update]: ChatTextArea

--- app/(public)/tests/usage-calculator/_components/ChatTextArea.tsx
+++ app/(public)/tests/usage-calculator/_components/ChatTextArea.tsx
@@ -7,14 +7,8 @@ interface ChatTextAreaProps extends React.ComponentProps<typeof Textarea> {}
 function ChatTextArea({ ...props }: ChatTextAreaProps) {
   return (
     <Textarea
-      onKeyDown={(e) => {
-        if (e.key === "Enter") {
-          e.preventDefault();
-          e.currentTarget.form?.requestSubmit();
-        }
-      }}
       placeholder="메시지를 입력하세요."
-      className="w-full h-full resize-none bg-neutral-800 text-sm text-neutral-100 placeholder:text-neutral-400"
+      className="w-full h-full resize-none bg-neutral-800 text-sm text-neutral-100 placeholder:text-neutral-400 focus-visible:ring-2"
       {...props}
     />
   );
