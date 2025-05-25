cf6b1ea82ff311da97e13fef90a9c832e6e30e9a
Eun Oh <bdohhhhh@gmail.com>
2025-05-25 21:44:01 +0900

    [update]: calculator page

--- app/(public)/tests/usage-calculator/page.tsx
+++ app/(public)/tests/usage-calculator/page.tsx
@@ -2,7 +2,7 @@ import UsageCalculatorTemplate from "./_components/UsageCalculatorTemplate";
 
 function UsageCalculatorPage() {
   return (
-    <div className="max-w-[684px] mx-auto h-full flex flex-col items-center justify-center">
+    <div className="max-w-[684px] mx-auto h-full flex flex-col items-center justify-center pt-4">
       <UsageCalculatorTemplate />
     </div>
   );
