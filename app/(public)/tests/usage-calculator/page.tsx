b3636904de4886a9968674f5a7fede6895b5ea69
Eun Oh <bdohhhhh@gmail.com>
2025-05-25 21:10:30 +0900

    [update]: calculator page

--- app/(public)/tests/usage-calculator/page.tsx
+++ app/(public)/tests/usage-calculator/page.tsx
@@ -1,7 +1,11 @@
 import UsageCalculatorTemplate from "./_components/UsageCalculatorTemplate";
 
 function UsageCalculatorPage() {
-  return <UsageCalculatorTemplate />;
+  return (
+    <div className="max-w-[684px] mx-auto h-full flex flex-col items-center justify-center">
+      <UsageCalculatorTemplate />
+    </div>
+  );
 }
 
 export default UsageCalculatorPage;
