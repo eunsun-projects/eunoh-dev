dff52e6fdcd9644fba3532792fa37828b5879a5c
Eun Oh <bdohhhhh@gmail.com>
2025-05-27 23:14:40 +0900

    [update]: calculate result

--- app/(public)/tests/usage-calculator/_components/CalculateResult.tsx
+++ app/(public)/tests/usage-calculator/_components/CalculateResult.tsx
@@ -15,36 +15,56 @@ function CalculateResult() {
   );
 
   const prices = useMemo(() => {
-    const input = usage?.promptTokens
-      ? base.input_txt_base.basePrice * usage.promptTokens
-      : 0;
-    const output = usage?.completionTokens
-      ? base.output_txt_base.basePrice * usage.completionTokens
-      : 0;
-    const total = input + output;
-
-    return {
-      inputPrice: Math.floor(input * 100000) / 100000,
-      outputPrice: Math.floor(output * 100000) / 100000,
-      totalPrice: Math.floor(total * 100000) / 100000,
+    const price = {
+      inputPrice: 0,
+      outputPrice: 0,
+      totalPrice: 0,
     };
+    if (!usage) {
+      return price;
+    }
+
+    if ("input_tokens" in usage) {
+      const input = base.input_txt_base.basePrice * usage.input_tokens;
+      const output = base.output_txt_base.basePrice * usage.output_tokens;
+      const total = input + output;
+      return {
+        inputPrice: Math.floor(input * 100000) / 100000,
+        outputPrice: Math.floor(output * 100000) / 100000,
+        totalPrice: Math.floor(total * 100000) / 100000,
+      };
+    }
+
+    if ("promptTokens" in usage) {
+      const input = base.input_txt_base.basePrice * usage.promptTokens;
+      const output = base.output_txt_base.basePrice * usage.completionTokens;
+      const total = input + output;
+      return {
+        inputPrice: Math.floor(input * 100000) / 100000,
+        outputPrice: Math.floor(output * 100000) / 100000,
+        totalPrice: Math.floor(total * 100000) / 100000,
+      };
+    }
+
+    return price;
   }, [usage, base]);
 
   return (
     <div className="w-full h-full flex gap-2 items-center justify-center text-neutral-200">
-      {usage?.promptTokens ? (
+      {/** vercel ai sdk 안쓴 api 호출시 */}
+      {usage ? (
         <p>인풋가격: {prices.inputPrice} 원</p>
       ) : (
         <Skeleton className="w-full h-4" />
       )}
       <Separator orientation="vertical" className="h-4 bg-neutral-200" />
-      {usage?.completionTokens ? (
+      {usage ? (
         <p>아웃풋가격: {prices.outputPrice} 원</p>
       ) : (
         <Skeleton className="w-full h-4" />
       )}
       <Separator orientation="vertical" className="h-4 bg-neutral-200" />
-      {usage?.promptTokens && usage?.completionTokens ? (
+      {usage ? (
         <p>총 가격: {prices.totalPrice} 원</p>
       ) : (
         <Skeleton className="w-full h-4" />
