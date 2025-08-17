ffe2a26347cf69b22a82e74dbfb84bbf82ccd2ff
EunOh <bdohhhhh@gmail.com>
2025-08-17 14:08:16 +0900

    update: apis/post/get.post

--- src/apis/post/get.post.ts
+++ src/apis/post/get.post.ts
@@ -1,8 +1,14 @@
 import type { Post } from "@/types/post.types";
 import fetchWrapper from "@/utils/common/fetchWrapper";
 
-export async function getPost(engTitle: string) {
-  const url = `/api/posts/${engTitle}`;
+interface GetPostResponse {
+  engTitle?: string;
+  id?: string;
+}
+
+export async function getPost({ engTitle, id }: GetPostResponse) {
+  const path = engTitle ? engTitle : id;
+  const url = `/api/posts/${path}`;
   try {
     const response = await fetchWrapper<Post>(url, {
       method: "GET",
