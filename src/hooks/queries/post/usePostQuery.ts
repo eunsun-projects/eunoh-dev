564ad30e33237146a54e50be83fffd2efb599ed4
EunOh <bdohhhhh@gmail.com>
2025-08-17 14:08:28 +0900

    update: usePostQuery

--- src/hooks/queries/post/usePostQuery.ts
+++ src/hooks/queries/post/usePostQuery.ts
@@ -2,9 +2,14 @@ import { getPost } from "@/apis/post";
 import { QUERY_KEY_POSTS } from "@/constants/query.constants";
 import { useQuery } from "@tanstack/react-query";
 
-export function usePostQuery(engTitle: string) {
+interface UsePostQueryProps {
+  engTitle?: string;
+  id?: string;
+}
+
+export function usePostQuery({ engTitle, id }: UsePostQueryProps) {
   return useQuery({
-    queryKey: [QUERY_KEY_POSTS, engTitle],
-    queryFn: () => getPost(engTitle),
+    queryKey: [QUERY_KEY_POSTS, engTitle, id],
+    queryFn: () => getPost({ engTitle, id }),
   });
 }
