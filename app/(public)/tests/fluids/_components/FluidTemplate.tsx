ef75a4e3d90d39598b3b87fd554d96fc75eb57c6
EunOh <bdohhhhh@gmail.com>
2025-10-05 05:23:46 +0900

    add: tests - fluid template

---
+++ app/(public)/tests/fluids/_components/FluidTemplate.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect } from "react";
+import FluidCanvas from "./FluidCanvas";
+
+export default function FluidTemplate() {
+  useEffect(() => {
+    const setVh = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+    setVh();
+    window.addEventListener("resize", setVh);
+    return () => window.removeEventListener("resize", setVh);
+  }, []);
+
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100dvh",
+        touchAction: "none",
+        background: "#0a0a0a",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        zIndex: 1000,
+      }}
+    >
+      <FluidCanvas />
+    </div>
+  );
+}
