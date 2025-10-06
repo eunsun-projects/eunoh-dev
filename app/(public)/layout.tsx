876abb3d69f45452686d25be1eaa009eecd6d19f
EunOh <bdohhhhh@gmail.com>
2025-10-07 01:16:43 +0900

    update: public layout (remove auth provider)

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -7,15 +7,13 @@ import Footer from "./_components/ui/Footer";
 function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <UiStateProvider>
-      <AuthProvider>
-        <OverScroll />
-        <div className="min-h-full">
-          <main className="max-w-[640px] mx-auto pt-[64px] min-h-[calc(100dvh-128px-28px)] xl:pt-[128px]">
-            {children}
-          </main>
-          <Footer />
-        </div>
-      </AuthProvider>
+      <OverScroll />
+      <div className="min-h-full">
+        <main className="max-w-[640px] mx-auto pt-[64px] min-h-[calc(100dvh-128px-28px)] xl:pt-[128px]">
+          {children}
+        </main>
+        <Footer />
+      </div>
     </UiStateProvider>
   );
 }
