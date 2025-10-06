d0f67dbf4c0b32bcd21009ff7676547d83318707
EunOh <bdohhhhh@gmail.com>
2025-10-07 01:14:39 +0900

    update: admin page > admin login page (apply prefetch)

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,9 +1,31 @@
-function AdminPage() {
+import { postUserServer } from "@/apis/auth/server/post.user";
+import { QUERY_KEY_USER } from "@/constants/query.constants";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
+import { redirect } from "next/navigation";
+import AdminLoginTemplate from "./_components/login-page-template";
+
+async function AdminLoginPage() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: [QUERY_KEY_USER],
+    queryFn: () => postUserServer(),
+  });
+
+  const user = await queryClient.getQueryData([QUERY_KEY_USER]);
+  if (user) return redirect("/admin/authed");
+
+  const dehydratedState = dehydrate(queryClient);
+
   return (
-    <div className="flex justify-center items-center px-8">
-      여기는 애드민 페이지 입니다
-    </div>
+    <HydrationBoundary state={dehydratedState}>
+      <AdminLoginTemplate />
+    </HydrationBoundary>
   );
 }
 
-export default AdminPage;
+export default AdminLoginPage;
