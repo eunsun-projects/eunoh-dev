1555e34b9ce587496fd5f018dcf5af0bf6a37057
EunOh <bdohhhhh@gmail.com>
2025-10-26 19:56:07 +0900

    fix: tests/circles 뒤로가기 버튼 수정

--- app/(public)/tests/circles/layout.tsx
+++ app/(public)/tests/circles/layout.tsx
@@ -1,18 +1,16 @@
-import Link from "next/link";
 import type { PropsWithChildren } from "react";
-import { RiArrowGoBackFill } from "react-icons/ri";
+import { Back } from "@/app/(public)/_components/ui";
 import CheckLandscape from "../paradise/_components/CheckLandscape";
 
 export default function CirclesLayout({ children }: PropsWithChildren) {
 	return (
 		<div className="fixed inset-0 z-50 h-screen w-screen bg-white">
-			<div className="fixed top-0 left-0 z-50 h-dvh w-dvw bg-transparent">
-				<Link href="/tests" className="absolute top-4 right-4 z-50">
-					<RiArrowGoBackFill className="cursor-pointer text-lg" />
-				</Link>
-				<CheckLandscape />
-				{children}
-			</div>
+			<Back
+				className="absolute top-1 right-1 z-[1001]"
+				iconClassName="text-black"
+			/>
+			<CheckLandscape />
+			{children}
 		</div>
 	);
 }
