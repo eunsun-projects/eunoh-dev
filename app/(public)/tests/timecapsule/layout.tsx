87f865025a26de583c7de54792e42c3eeba29cc1
EunOh <bdohhhhh@gmail.com>
2025-10-26 19:57:32 +0900

    update: tests/timecapsule 뒤로가기 추가

--- app/(public)/tests/timecapsule/layout.tsx
+++ app/(public)/tests/timecapsule/layout.tsx
@@ -1,8 +1,16 @@
+import type { Metadata } from "next";
 import type { PropsWithChildren } from "react";
+import { Back } from "../../_components/ui";
+
+export const metadata: Metadata = {
+	title: "TimeCapsule",
+	description: "TimeCapsule",
+};
 
 export default function TimeCapsuleLayout({ children }: PropsWithChildren) {
 	return (
 		<div className="fixed inset-0 z-50 h-screen w-screen bg-black">
+			<Back className="absolute top-1 left-1 z-50" />
 			{children}
 		</div>
 	);
