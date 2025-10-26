af855622ce26bfcbd03546eaac0854614bd1c53c
EunOh <bdohhhhh@gmail.com>
2025-10-26 13:00:13 +0900

    update: circles 에도 뒤로가기 버튼 추가

--- app/(public)/tests/circles/layout.tsx
+++ app/(public)/tests/circles/layout.tsx
@@ -1,9 +1,18 @@
+import Link from "next/link";
 import type { PropsWithChildren } from "react";
+import { RiArrowGoBackFill } from "react-icons/ri";
+import CheckLandscape from "../paradise/_components/CheckLandscape";
 
 export default function CirclesLayout({ children }: PropsWithChildren) {
 	return (
 		<div className="fixed inset-0 z-50 h-screen w-screen bg-white">
-			{children}
+			<div className="fixed top-0 left-0 z-50 h-dvh w-dvw bg-transparent">
+				<Link href="/tests" className="absolute top-4 right-4 z-50">
+					<RiArrowGoBackFill className="cursor-pointer text-lg" />
+				</Link>
+				<CheckLandscape />
+				{children}
+			</div>
 		</div>
 	);
 }
