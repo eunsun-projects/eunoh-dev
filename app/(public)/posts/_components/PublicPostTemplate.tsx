f3ed5cbf4c1b0b883a07aa13fc845fd71294e578
EunOh <bdohhhhh@gmail.com>
2025-12-14 16:29:12 +0900

    style: style 통일 및 날짜표기 방식 변경

--- app/(public)/posts/_components/PublicPostTemplate.tsx
+++ app/(public)/posts/_components/PublicPostTemplate.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import "highlight.js/styles/a11y-dark.css";
+import { format } from "date-fns";
 import ReactMarkdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
 import remarkGfm from "remark-gfm";
@@ -16,9 +17,14 @@ function PublicPostTemplate({ post }: PublicPostTemplateProps) {
 	return (
 		<section className="flex flex-col gap-8">
 			<div className="flex w-full justify-between">
-				<h2 className="m-0 font-bold text-lg text-neutral-900 dark:text-neutral-50">
-					{post.title}
-				</h2>
+				<div className="flex flex-row items-center gap-4">
+					<h2 className="m-0 font-bold text-lg text-neutral-900 dark:text-neutral-50">
+						{post.title}
+					</h2>
+					<p className="m-0 text-sm">
+						{format(new Date(post.posted_at as string), "yyyy.MM.dd")}
+					</p>
+				</div>
 				<Back isDarkLightModeButton />
 			</div>
 			<div>
--- app/(public)/posts/_components/PublicPostsListTemplate.tsx
+++ app/(public)/posts/_components/PublicPostsListTemplate.tsx
@@ -40,16 +40,16 @@ function PublicPostsListTemplate() {
 										href={`/posts/${post.engTitle}`}
 										className="flex flex-row gap-0.5"
 									>
-										<h3 className="m-0 w-fit p-0.5 text-neutral-900 text-xs hover:bg-neutral-300 xl:text-sm dark:text-neutral-50 dark:hover:bg-neutral-500">
+										<p className="m-0 w-fit p-0.5 text-neutral-900 text-xs hover:bg-neutral-300 dark:text-neutral-50 dark:hover:bg-neutral-500">
 											{`(${post.category}) ${post.title}`}
-										</h3>
+										</p>
 									</Link>
 								</div>
-								<p className="m-0 text-[10px] xl:text-xs">{post.summary}</p>
+								<p className="m-0 text-[10px]">{post.summary}</p>
 							</div>
 							<div className="flex">
 								<p className="m-0 text-sm">
-									{format(new Date(post.posted_at as string), "yyyy")}
+									{format(new Date(post.posted_at as string), "yyyy.MM")}
 								</p>
 							</div>
 						</div>
