5e17e7e4d7ff454ac395acd0f3c33f0fe04b58b1
EunOh <bdohhhhh@gmail.com>
2026-02-18 11:18:32 +0900

    chore: route handler 위치이동

--- app/(public)/tests/amaechild/_apis/apis.ts
+++ app/(public)/tests/amaechild/_apis/apis.ts
@@ -2,15 +2,15 @@ import axios from "@/lib/axios";
 import type { AllGiftsResponse, Iffy } from "../_types/types";
 
 export const getAllIffies = async () => {
-	return axios.get<AllGiftsResponse>("/api/iffy");
+	return axios.get<AllGiftsResponse>("/api/tests/iffy");
 };
 
 export const getIffy = async ({ id }: { id: string }) => {
-	return axios.get<Iffy>(`/api/iffy?id=${id}`);
+	return axios.get<Iffy>(`/api/tests/iffy?id=${id}`);
 };
 
 export const postIffy = async ({ formData }: { formData: FormData }) => {
-	return axios.post<Iffy>("/api/iffy", formData, {
+	return axios.post<Iffy>("/api/tests/iffy", formData, {
 		headers: {
 			"Content-Type": "multipart/form-data",
 		},
